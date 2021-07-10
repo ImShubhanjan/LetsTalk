e8a603749899d3d552331a6cca8cf49abfeca100
Shubhanjan Barai <shubhanjanbarai18@gmail.com>
2021-07-10 21:19:35 +0530

    Added Server

--- server.js
+++ server.js
@@ -57,4 +57,4 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 3001 ;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
